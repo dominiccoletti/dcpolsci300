c4e34eff8b94a0cdd1db86a66ce16ead6a3c7a5c
Dominic Coletti <dcoletti@umich.edu>
2021-10-22 12:46:55 -0400

    Add q1b to Asst 5

--- Assignment 5.R
+++ Assignment 5.R
@@ -8,7 +8,9 @@ acddata$smd <- ifelse(acddata$lml==0,1,0)
 #Q1a Estimate
 model1a <- lm(enep ~ smd, data=acddata)
 summary(model1a)
+#Q1b p-level
 beta.est <- model1a$coefficients[2]
 h0.value <- 0
 beta.se <- summary(model1a)$coefficients[2,2]
 t.stat <- (beta.est - h0.value)/beta.se
+2*pt(-abs(t.stat), model1a$df.residual)
